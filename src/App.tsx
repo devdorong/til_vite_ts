41ec5e0286cfdfdeecc38c808b06d695c720cfcc
devdorong <devdorong@gmail.com>
2025-09-03 13:20:28 +0900

    [docs] auth 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,66 @@
-import React from 'react';
-import { TodoProvider } from './contexts/TodoContext';
-import TodoWrite from './components/todos/TodoWrite';
-import TodoList from './components/todos/TodoList';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { AuthProvider, useAuth } from './contexts/AutoContext';
+import AuthCallback from './pages/AuthCallback';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import SigninPage from './pages/SigninPage';
+import TodosPage from './pages/TodosPage';
+import Protected from './components/Protected';
+
+const TopBar = () => {
+  const { signOut, user } = useAuth();
+  return (
+    <nav
+      style={{
+        display: 'flex',
+        gap: '20px',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        margin: '0 auto',
+        padding: '20px',
+      }}
+    >
+      <Link to={'/'}>홈</Link>
+
+      {user ? (
+        <>
+          <Link to={'/todos'}>할일</Link>
+          <button onClick={signOut}>로그아웃</button>
+        </>
+      ) : (
+        <>
+          <Link to={'/signup'}>회원가입</Link>
+          <Link to={'/signin'}>로그인</Link>
+        </>
+      )}
+    </nav>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <h1>Todo Service</h1>
-      <TodoProvider>
-        <TodoWrite />
-        <TodoList />
-      </TodoProvider>
-    </div>
+    <AuthProvider>
+      <div>
+        <h1>Todo Service</h1>
+        <Router>
+          <TopBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/signin" element={<SigninPage />} />
+            <Route path="/auth/callback" element={<AuthCallback />} />
+            <Route
+              path="/todos"
+              element={
+                <Protected>
+                  <TodosPage />
+                </Protected>  
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
+    </AuthProvider>
   );
 }
 
---
+++ src/pages/SigninPage.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { useAuth } from '../contexts/AutoContext';
+
+function SigninPage() {
+  const { signIn } = useAuth();
+  const [email, setEmail] = useState<string>('');
+  const [pw, setPw] = useState<string>('');
+  const [msg, setMsg] = useState<string>('');
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { error } = await signIn(email, pw);
+    if (error) {
+      setMsg(`로그인 오류 : ${error}`);
+    } else {
+      setMsg('로그인 성공');
+    }
+  };
+  return (
+    <div>
+      <h2>로그인 페이지</h2>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+          <input type="password" value={pw} onChange={e => setPw(e.target.value)} />
+          <button type="submit">로그인</button>
+        </form>
+        <p>{msg}</p>
+      </div>
+    </div>
+  );
+}
+
+export default SigninPage;
